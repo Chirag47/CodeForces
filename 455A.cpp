4cede6d7a30c62c80921d307b2db3cf99e776ca0
Chirag Agrawal <chiragagrawal65@gmail.com>
2017-10-23 23:05:25 +0530

    DID 2 questions today

---
+++ 455A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+typedef struct{
+    int w;
+    int i;
+}number;
+int main(){
+    int n;
+    cin>>n;
+    vector<number> a;
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        for()
+    }
+    int pg = a[0];
+    int pl = a[1];
+    int max = 0;
+    int pos = -1;
+    if(pg > pl){
+        max = pg;
+        pos = 0;
+    }
+}
